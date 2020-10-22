ec5d0429d80000ca20cb1cb4d4dd3f3da10eeba0
victor wang <40143704+wccvictor40@users.noreply.github.com>
2020-10-23 00:43:32 +0900

    Create Services.java

---
+++ src/Service/Services.java
@@ -0,0 +1,43 @@
+package Service;
+
+import java.util.ArrayList;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ *
+ * @author wang cc
+ */
+public class Services {
+
+    public String filepath = "files//Event_file.xml";
+    public String filepath_users = "files//User_file.xml";
+    public String filepath_programs = "files//Program_file.xml";
+    public String filepath_event_attend = "files//Attendance.xml";
+
+    ArrayList<String> event_list = new ArrayList<>();
+    ArrayList<String> user_list = new ArrayList<>();
+    ArrayList<String> program_list = new ArrayList<>();
+    
+    public Services() {
+
+    }
+
+    public boolean check_program(String name) {
+        return false;
+    }
+
+    public void list_all_lecturers() {
+
+    }
+
+    public void list_all_users() {
+    }
+    
+    public static String getTagValue(String sTag, Element eElement) {
+        NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
+        Node nValue = (Node) nlList.item(0);
+        return nValue.getNodeValue();
+    }
+}
